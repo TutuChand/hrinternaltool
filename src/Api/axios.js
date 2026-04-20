bc5d72049afb72471fd56708a5ef44044c45c096
TutuDucktale <tutuchando@ducktaleit.com>
2026-04-20 17:16:49 +0530

    fix API routes to match proxy setup

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://niyamra.ducktaleit.com",
   headers: {
     "Content-Type": "application/json",
   },
